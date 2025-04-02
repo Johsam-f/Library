68373f2aecf1082f3237cb61a66adf8a5d5bee18
Johsam <johsamfcress@gmail.com>
2025-04-02 11:29:05 +0200

    extra credit

--- js/script.js
+++ js/script.js
@@ -33,7 +33,7 @@ const myLibrary = [
         <p><span class="blue">No of Pages:</span> <span class="page-number">${book.pages}</span></p>
         <p><span class="blue">Status:</span> <span class="status">${book.status}</span></p>
         <div class="btn-container flex">
-          <button class="btn edit-book-btn" onclick="edit_book()">Edit</button>
+          <button class="btn edit-book-btn" onclick="change_status()">Change status</button>
           <button class="btn remove-book-btn" onclick="delete_book()">
             <i class="fas fa-trash"></i>
           </button>
@@ -54,3 +54,24 @@ const myLibrary = [
   }
 
   document.addEventListener('DOMContentLoaded', put_books_on_shelf);
+
+  // the button functions
+
+  //add book button
+  function popup_menu(){
+    let hide_status = document.getElementById("add-book");
+    const rotate_btn = document.querySelector(".add-btn");
+
+    if(hide_status.classList == "hidden"){
+      hide_status.classList.remove('hidden');
+      rotate_btn.style.transform = "rotate(45deg)";
+    }else{
+      hide_status.classList.add('hidden');
+      rotate_btn.style.transform = "rotate(360deg)";
+    }
+  }
+
+  //change status button
+  function change_status(){
+    
+  }
